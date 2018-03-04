027b07241c49031a10895b1602e173a80b522514
YcMarch <wo2_dong@126.com>
2018-03-04 16:04:51 +0800

    切云盘第一个页面，下一次切右侧下部分

---
+++ js/js.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+
+    var body = document.querySelector('body');
+    // 给 body 设置高度 为 浏览器可视高度
+    body.style.height = document.documentElement.clientHeight +'px';
+
+
+
+
+}
